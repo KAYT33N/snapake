1b782d6a9102860cf40befaf582151d71b5848db
KAYT33N <K80arabi@gmail.com>
2024-02-20 01:18:15 +0330

    main fn & What i expect of world::World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+#[allow(unused_imports)]
+use std::{
+    thread,
+    time
+};
+use ncurses::{*};
+mod world;
+use world::{Point, World};
+
 fn main() {
-    println!("Hello, world!");
+    // Start ncurses.
+    let screen = initscr();
+    clear();
+    refresh();
+    noecho();
+    cbreak();
+    nodelay(screen, true);
+    // Get max col & row number
+    let mut maxx : i32 = 0;
+    let mut maxy : i32 = 0;
+    getmaxyx(screen, &mut maxy, &mut maxx);
+    // Main game Loop
+    loop {
+        // Creates new world
+        let world = World::new(
+            // Pass maximum x & y
+            Point{
+                x: maxx as usize, 
+                y: maxy as usize
+            },
+            // Number of foods
+            5, 
+            // Number of enemies
+            3, 
+            // Chance of turning a space into stone
+            0.03
+        );
+        // Limit FPS
+        let fps = 10;
+        let pause = 1000/fps;
+        // A Round's loop
+        while world.is_playing {
+            // Pass pressed key to world to proccess what to do
+            // move/reset/quit
+            world.tick(Into::<char>::into(getch() as u8));
+            // Render to terminal
+            world.render(screen);
+            // Apply FPS limit
+            thread::sleep(time::Duration::from_millis(pause));
+        }
+        // Show results
+        // world.res();
+        if Into::<char>::into(getch() as u8) == 'q' {
+            break;
+        }
+    }
+    /* Terminate ncurses. */
+    endwin();
 }
