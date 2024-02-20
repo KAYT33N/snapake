3bdd10be6a97d7c859cfdaed9ce0114c13bf4292
KAYT33N <K80arabi@gmail.com>
2024-02-20 19:28:48 +0330

    Making Game struct

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,13 @@
+use rand::prelude::*;
+use ncurses::{
+    getmaxyx,
+    addstr,
+    addch,
+    wmove,
+    clear,
+    refresh
+};
+
 #[derive(Clone, Copy)]
 pub struct Config {
     pub fps      : usize,
@@ -45,4 +55,65 @@ impl Into<u32> for Types {
             Self::Stone  => '.' as u32,
         }
     }
+}
+
+// Main game's struct
+pub struct Game {
+    pub is_playing: bool,
+    pub config    : Config,
+
+    screen  : ncurses::WINDOW,
+    maxx    : usize,
+    maxy    : usize,
+    player  : Point,
+    foods   : Vec<Food>,
+    world   : Vec<Vec<Types>>,
+    enemies : Vec<Point>,
+    score   : usize,
+    tip     : String
+}
+
+impl Game{
+    // creates 
+    pub fn new(
+        screen      : ncurses::WINDOW, 
+        last_config : Option<Config>
+      ) -> Self {
+            // get max x & y
+            let mut maxx : i32 = 0;
+            let mut maxy : i32 = 0;
+            getmaxyx(screen, &mut maxy, &mut maxx);
+            // create empty game
+            let mut tmp = Self{
+                is_playing  : true,
+                config      : Self::get_config(last_config),
+                screen      : screen,
+                maxx        : maxx as usize,
+                maxy        : maxy as usize,
+                player      : Point{x:0, y:0},
+                foods       : Vec::new(),
+                world       : Vec::new(),
+                enemies     : Vec::new(),
+                score       : 0,
+                tip         : String::from("Press `q` to exit  |  use 'wasd' to move")
+            };
+            // fill it
+            // tmp.init();
+            // return game
+            tmp
+    }
+
+    // Shows user graphical screen to change values
+    fn get_config(last_config: Option<Config>) -> Config {
+        Config {
+            enemies_count: 3,
+            foods_count  : 5,
+            stones_chance: 0.03,
+            enemies_level: 0.15,
+            foods_min_age: 4,
+            foods_max_age: 7,
+            fps          : 30
+        }
+    }
+
 }
